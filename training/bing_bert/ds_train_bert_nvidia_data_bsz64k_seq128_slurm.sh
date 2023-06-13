5c1fb2b91418e6f42565ed796ad66275460981f3
NanoNabla <43477372+NanoNabla@users.noreply.github.com>
2023-06-13 13:29:30 +0200

    add slurm start script

---
+++ training/bing_bert/ds_train_bert_nvidia_data_bsz64k_seq128_slurm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+base_dir=`pwd`
+
+# Where should we save checkpoints and tensorboard events?
+JOB_NAME=lamb_nvidia_data_64k_seq128
+OUTPUT_DIR=${base_dir}/bert_model_nvidia_data_outputs
+
+DATA_PATH="/workspace/bert"
+
+
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+export WORLD_SIZE=$SLURM_NTASKS
+
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+
+
+mkdir -p $OUTPUT_DIR
+
+NCCL_TREE_THRESHOLD=0 python ${base_dir}/deepspeed_train.py \
+--cf ${base_dir}/bert_large_lamb_nvidia_data.json \
+--max_seq_length 128 \
+--output_dir $OUTPUT_DIR \
+--deepspeed \
+--deepspeed_transformer_kernel \
+--print_steps 100 \
+--lr_schedule "EE" \
+--lr_offset 10e-4 \
+--job_name $JOB_NAME \
+--deepspeed_config ${base_dir}/deepspeed_bsz64k_lamb_config_seq128.json \
+--data_path_prefix $DATA_PATH \
+--use_nvidia_dataset 
