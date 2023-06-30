60ce79ffa0c6d1d1585435b7eac7b76fee9fd814
Alexander Grund <alexander.grund@tu-dresden.de>
2023-06-30 16:18:14 +0200

    Improve `ds_train_bert_nvidia_data_bsz64k_seq128_slurm.sh`
    Apply proper quoting and getting of `base_dir`, i.e. path of the script which may not be PWD.

--- training/bing_bert/ds_train_bert_nvidia_data_bsz64k_seq128_slurm.sh
+++ training/bing_bert/ds_train_bert_nvidia_data_bsz64k_seq128_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-base_dir=`pwd`
+base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Where should we save checkpoints and tensorboard events?
 JOB_NAME=lamb_nvidia_data_64k_seq128
-OUTPUT_DIR=${base_dir}/bert_model_nvidia_data_outputs
+OUTPUT_DIR=$(pwd)/bert_model_nvidia_data_outputs
 
 DATA_PATH="/workspace/bert"
 
@@ -18,18 +18,18 @@ master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 
 
-mkdir -p $OUTPUT_DIR
+mkdir -p "$OUTPUT_DIR"
 
-NCCL_TREE_THRESHOLD=0 python ${base_dir}/deepspeed_train.py \
---cf ${base_dir}/bert_large_lamb_nvidia_data.json \
+NCCL_TREE_THRESHOLD=0 python "${base_dir}/deepspeed_train.py" \
+--cf "${base_dir}/bert_large_lamb_nvidia_data.json" \
 --max_seq_length 128 \
---output_dir $OUTPUT_DIR \
+--output_dir "$OUTPUT_DIR" \
 --deepspeed \
 --deepspeed_transformer_kernel \
 --print_steps 100 \
 --lr_schedule "EE" \
 --lr_offset 10e-4 \
---job_name $JOB_NAME \
---deepspeed_config ${base_dir}/deepspeed_bsz64k_lamb_config_seq128.json \
---data_path_prefix $DATA_PATH \
+--job_name "$JOB_NAME" \
+--deepspeed_config "${base_dir}/deepspeed_bsz64k_lamb_config_seq128.json" \
+--data_path_prefix "$DATA_PATH" \
 --use_nvidia_dataset 
