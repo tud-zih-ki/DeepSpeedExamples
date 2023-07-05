01aff03f1ed1a0f57405345b919af463ff28c8a7
Alexander Grund <alexander.grund@tu-dresden.de>
2023-07-05 15:46:53 +0200

    Calculate and display time to reach target loss
    Add a function to find the time when the loss dropped below a threshold
    for a minimum number of iterations and print that value rounded to 2
    decimals.

--- training/bing_bert/export_from_tensorboard.py
+++ training/bing_bert/export_from_tensorboard.py
@@ -4,6 +4,7 @@ from tbparse import SummaryReader
 import pandas as pd
 import argparse
 
+RUNTIME_NAME = 'Runtime (sec)'
 
 def parse_tb2(logdir, csv_out):
     reader = SummaryReader(logdir, extra_columns=set(['wall_time']))
@@ -13,7 +14,7 @@ def parse_tb2(logdir, csv_out):
 
     loss_df['wall_clock'] = pd.to_datetime(loss_df.wall_time, unit="s")
     first = loss_df.wall_time.iloc[0]
-    loss_df['Runtime (sec)'] = loss_df.wall_time - first
+    loss_df[RUNTIME_NAME] = loss_df.wall_time - first
 
     loss_df.rename(columns={'step': 'Samples', 'value': 'Loss'}, inplace=True)
     loss_df.drop(columns=['tag', 'wall_time', 'wall_clock'], inplace=True)
@@ -24,10 +25,23 @@ def parse_tb2(logdir, csv_out):
     return loss_df
 
 
+def find_runtime(df, min_iters, loss_limit):
+    for d in df.rolling(min_iters, method="table"):
+        if d.shape[0] == min_iters and (d['Loss'] < loss_limit).all():
+            return d.iloc[-1][RUNTIME_NAME]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse & print TensorBoard event files')
     parser.add_argument('logdir', help='TensorBoard logdir')
     parser.add_argument('--csv_out', help='csv output file')
     args = parser.parse_args()
 
-    print(parse_tb2(args.logdir, args.csv_out))
+    data = parse_tb2(args.logdir, args.csv_out)
+    print(data)
+    print('='*60)
+    target_runtime = find_runtime(data, min_iters=2, loss_limit=8.5)
+    if target_runtime:
+        print(f"Time to reach target loss: {target_runtime:.2f} s")
+    else:
+        print("Target loss not reached")
