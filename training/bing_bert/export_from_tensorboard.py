afe687aff8e19e8067a652d8874769b75ca0ef45
NanoNabla <43477372+NanoNabla@users.noreply.github.com>
2023-07-11 17:25:36 +0200

    print accepted runtime in export_from_tensorboard.py

--- training/bing_bert/export_from_tensorboard.py
+++ training/bing_bert/export_from_tensorboard.py
@@ -4,7 +4,7 @@ import argparse
 
 
 
-def parse_tb2(logdir, csv_out):
+def parse_tb2(logdir, csv_out, threshold, min_iter):
 	reader = SummaryReader(logdir, extra_columns=set(['wall_time']))
 	df = reader.scalars
 
@@ -16,23 +16,41 @@ def parse_tb2(logdir, csv_out):
 	loss_df.rename(columns = {'step' : 'Samples', 'value' : 'Loss'}, inplace=True)
 	loss_df.drop(columns = ['tag', 'wall_time', 'wall_clock'], inplace=True)
 	
+	target_runtime_idx = find_runtime(loss_df, threshold, min_iter)
 	
 	if csv_out:
-#		df[df['tag'] == 'Train/Samples/train_loss'].to_csv(csv_out)
 		loss_df.to_csv(csv_out)
 	
-	return loss_df
+	return loss_df, target_runtime_idx
 
 	
 	
-	
-	
+def find_runtime(df, threshold, min_iter):
+	for i in df.index:
+		if df['Loss'][i]  < threshold:
+			val = True
+			for j in range(min_iter+1):
+				val = val and (df['Loss'][i+j] < threshold)
+			if val:
+				return i
+		if i == df.index[-min_iter-1]:
+			return False
+
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Parse TensorBoard Event files and prints them')
 	parser.add_argument('logdir', help='TensorBoard logdir')
-	parser.add_argument('--csv_out', help='csv output file', required=False, default=None)
+	parser.add_argument('--csv_out', help='csv output file', default=None)
+	parser.add_argument('--threshold', help='threadhold values are acceptable', default=8.5, type=float)
+	parser.add_argument('--min_iter', help='minimum iteration under the threshold', default=2, type=int)
+
 	args = parser.parse_args()
 
-	print(parse_tb2(args.logdir, args.csv_out))
+	df, target_runtime_idx = parse_tb2(args.logdir, args.csv_out, args.threshold, args.min_iter)
+	print(df.to_string())
+	print('='*60)
+	if target_runtime_idx:
+		print(f"Time to reach target loss: {df['Runtime'][target_runtime_idx]:.2f} s")
+	else:
+		print("Target loss not reached")
